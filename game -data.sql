4506f430ab6e19296df4a1bee0912ea386268d72
Mark Blumenthal <145574315+MarkBlumenthal@users.noreply.github.com>
2024-03-21 10:10:52 +0200

    Add files via upload

---
+++ game -data.sql
@@ -0,0 +1,25 @@
+-- Database: Demon Slayer
+
+-- DROP DATABASE IF EXISTS "Demon Slayer";
+
+-- CREATE DATABASE "Demon Slayer"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'English_South Africa.1252'
+--     LC_CTYPE = 'English_South Africa.1252'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+
+-- CREATE TABLE player_data (
+--     id SERIAL PRIMARY KEY,
+--     player_name VARCHAR(255),
+--     character_name VARCHAR(255),
+--     play_time TIMESTAMP
+-- );
+
+SELECT * FROM player_data;
+
